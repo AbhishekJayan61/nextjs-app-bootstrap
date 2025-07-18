d35a84f0aa9901f62a3f386ad3c3b264e1a35a2b
AbhishekJayan61 <131273793+AbhishekJayan61@users.noreply.github.com>
2025-07-18 16:13:08 +0530

    Add .next/static/chunks/src_app_admin_jobs_page_tsx_7868247e._.js 1752835388582

---
+++ .next/static/chunks/src_app_admin_jobs_page_tsx_7868247e._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_4903c4ae._.js",
+  "static/chunks/node_modules_58fd0e07._.js"
+],
+    source: "dynamic"
+});
